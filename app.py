a08b45350cd5be50e5a0275aac5698b95ab12781
hrehouma <rehoumahaythem@gmail.com>
2023-12-13 18:08:02 -0500

    ajout de la librairie matplolt

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
